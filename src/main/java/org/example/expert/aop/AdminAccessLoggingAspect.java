bc4517df32884b4673e2608fce6816592345233e
ljh <leejunghyun1019@gmail.com>
2026-04-02 21:33:00 +0900

    feat: AOP가 changeUserRole 실행 전 동작하도록 수정

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,8 +18,11 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
-    public void logAfterChangeUserRole(JoinPoint joinPoint) {
+    // 실행 전 동작해야 하므로 Before로 변경
+    // 포인트컷: AOP에서 "어디에 부가 기능을 적용할지"를 지정하는 것으로 해당 메서드가 실행되면 함께 실행할 코드로 지정하는 것
+    // 메서드 이름 통일 After -> Before
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    public void logBeforeChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
