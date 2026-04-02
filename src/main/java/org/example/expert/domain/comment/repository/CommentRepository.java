3b2e2aff749d074923237c2f190801b706139652
ljh <leejunghyun1019@gmail.com>
2026-04-03 05:31:13 +0900

    fix: N+1 문제 해결을 위한 JOIN FETCH 적용

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // 기존 JOIN c.user 만 되어 있어 Comment를 가져온 후 각 Comment 마다 User를 따로 조회하는 N+1 발생
+    // Join -> Join FETCH로 한번에 가져오게 바꿈
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
